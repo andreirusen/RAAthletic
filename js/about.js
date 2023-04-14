ab73d51d4839a900acb9431b2051b32b725baf04
Volkerus <andrei.rusen54@gmail.com>
2023-04-14 23:36:18 +0300

    update-14.04.2023

--- js/about.js
+++ js/about.js
@@ -7,26 +7,36 @@ let fgImg3 = document.querySelector('.fg3-programs-img');
 let circleLeft = document.querySelector('.circle-left-img');
 let circleRight = document.querySelector('.circle-right-img');
 
-let eclipseLeft = document.querySelector('.eclipse-left-img');
-
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
-    headerAbout.style.marginTop = value * 1.1 + 'px'; 
+    headerAbout.style.marginTop = value * 1 + 'px'; 
     fgImg1.style.marginTop = value * 0.3 + 'px';
     fgImg2.style.marginTop = value * 0.8 + 'px';
     fgImg3.style.marginLeft = value * 0.3 + 'px';
     circleLeft.style.marginRight = value * 0.3 + 'px';
     circleRight.style.marginLeft = value * 0.3 + 'px';
-    eclipseLeft.style.marginRight = value * 1.1 + 'px';
-    fgAthlet.style.marginRight = value * 1.1 + 'px';
+    fgAthlet.style.marginRight = value * 1 + 'px';
 
 });
 
-// const scrollToContent = document.querySelector('#btn-let');
-// scrollToContent.addEventListener("click", function () {
-//     window.scrollTo({
-//       bottom: 25%,
-//       behavior: "smooth",
-//     });
-//   });
+let scrollToTopButton = document.querySelector('.btn-let')
+scrollToTopButton.addEventListener("click", function () {
+    // Functia scrollTo este o functie predefinita.
+    window.scrollTo({
+      bottom: 0,
+      behavior: "smooth",
+    });
+  });
+
+
+
+  let btnLetBegin = document.querySelector('.btn-let-begin')
+  document.addEventListener("scroll", function () {
+    // Proprietatea scrollY reprezinta numarul de pizeli scrollati pe axa OY (cand nu am scrollat deloc este 0).
+    if (window.scrollY > 700) {
+        btnLetBegin.style.visibility = "visible";
+    } else {
+        btnLetBegin.style.visibility = "hidden";
+    }
+  });
--- js/index.js
+++ js/index.js
@@ -5,17 +5,23 @@ var typed = new Typed ('#typed-strings', {
   loop:true
 });
 
-
-
-
 let leftSection = document.querySelector('.left-section')
 let showcaseImg = document.querySelector('.showcase-img');
-let c1 = document.querySelector('#content-1')
-let c2 = document.querySelector('#content-2')
-
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
-    leftSection.style.marginRight = value * 1.2 + 'px'; 
-    showcaseImg.style.marginLeft = value * 1.2 + 'px'; 
+    leftSection.style.marginRight = value * 1 + 'px'; 
+    showcaseImg.style.marginLeft = value * 1 + 'px'; 
+});
+
+
+let btnLetBegin = document.querySelector('.btn-let-begin')
+
+document.addEventListener("scroll", function () {
+  // Proprietatea scrollY reprezinta numarul de pizeli scrollati pe axa OY (cand nu am scrollat deloc este 0).
+  if (window.scrollY > 500) {
+      btnLetBegin.style.visibility = "visible";
+  } else {
+      btnLetBegin.style.visibility = "hidden";
+  }
 });
