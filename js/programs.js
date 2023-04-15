ab7f515419955a7707ba3d0d211fb97dab36d3af
Volkerus <andrei.rusen54@gmail.com>
2023-04-16 02:00:10 +0300

    card created in programs

--- js/programs.js
+++ js/programs.js
@@ -1,24 +1,31 @@
-let bgImg = document.querySelector('.bg-programs-img');
-let headerPrograms = document.querySelector('.header-programs');
-let btnPrograms = document.querySelector('.btn-let');
-let btnLetBegin = document.querySelector('.btn-let-begin');
-let fgImg1 = document.querySelector('.fg1-programs-img');
-let fgImg3 = document.querySelector('.fg3-programs-img');
+let bgImg = document.querySelector(".bg-programs-img");
+let headerPrograms = document.querySelector(".header-programs");
+let btnPrograms = document.querySelector(".btn-programs");
+let btnLetBegin = document.querySelector(".btn-let-begin");
+let fgImg1 = document.querySelector(".fg1-programs-img");
+let fgImg3 = document.querySelector(".fg3-programs-img");
+let programSection = document.querySelector(".programs-card-section");
 
-window.addEventListener('scroll', function() {
-    let value = window.scrollY;
-    headerPrograms.style.marginBottom = value * 1.1+ 'px'; 
-    btnPrograms.style.marginTop = value * 1.1 + 'px'; 
-    fgImg1.style.marginTop = value * 0.3 + 'px';
-    fgImg3.style.marginLeft = value * 0.3 + 'px';
+// Scroll To Section
+function scrollToPrograms() {
+    programSection.scrollIntoView({ behavior: "smooth" });
+}
 
+// Parallax Effect
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  headerPrograms.style.marginBottom = value * 1.1 + "px";
+  btnPrograms.style.marginTop = value * 1.1 + "px";
+  fgImg1.style.marginTop = value * 0.3 + "px";
+  fgImg3.style.marginLeft = value * 0.3 + "px";
 });
 
+// Let's Begin BTN
 document.addEventListener("scroll", function () {
   // Proprietatea scrollY reprezinta numarul de pizeli scrollati pe axa OY (cand nu am scrollat deloc este 0).
   if (window.scrollY > 700) {
-      btnLetBegin.style.visibility = "visible";
+    btnLetBegin.style.visibility = "visible";
   } else {
-      btnLetBegin.style.visibility = "hidden";
+    btnLetBegin.style.visibility = "hidden";
   }
 });
