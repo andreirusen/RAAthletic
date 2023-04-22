9a79b7f7a4efe8209cad567b48ea4bf7206981fe
Andrei Rusen <andrei.rusen54@gmail.com>
2023-04-22 21:01:28 +0300

    scroll to top button

--- js/about.js
+++ js/about.js
@@ -1,47 +1,65 @@
-let headerAbout = document.querySelector('.header-about');
-let bgImg = document.querySelector('.bg-programs-img');
-let fgAthlet = document.querySelector('.fg-athlet-programs-img');
-let fgImg1 = document.querySelector('.fg1-programs-img');
-let fgImg3 = document.querySelector('.fg3-programs-img');
-let circleLeft = document.querySelector('.circle-left-img');
-let circleRight = document.querySelector('.circle-right-img');
-let scrollToTopButton = document.querySelector('.btn-let')
-let btnLetBegin = document.querySelector('.btn-let-begin')
+let headerAbout = document.querySelector(".header-about");
+let bgImg = document.querySelector(".bg-programs-img");
+let fgAthlet = document.querySelector(".fg-athlet-programs-img");
+let fgImg1 = document.querySelector(".fg1-programs-img");
+let fgImg3 = document.querySelector(".fg3-programs-img");
+let circleLeft = document.querySelector(".circle-left-img");
+let circleRight = document.querySelector(".circle-right-img");
+let findOut = document.querySelector(".btn-about");
 
-window.addEventListener('scroll', function() {
-    let value = window.scrollY;
-    headerAbout.style.marginTop = value * 1 + 'px'; 
-    fgImg1.style.marginTop = value * 0.3 + 'px';
-    fgImg3.style.marginLeft = value * 0.3 + 'px';
-    circleLeft.style.marginRight = value * 0.3 + 'px';
-    circleRight.style.marginLeft = value * 0.3 + 'px';
-    fgAthlet.style.marginRight = value * 1 + 'px';
-
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  headerAbout.style.marginTop = value * 1 + "px";
+  fgImg1.style.marginTop = value * 0.3 + "px";
+  fgImg3.style.marginLeft = value * 0.3 + "px";
+  circleLeft.style.marginRight = value * 0.3 + "px";
+  circleRight.style.marginLeft = value * 0.3 + "px";
+  fgAthlet.style.marginRight = value * 1 + "px";
 });
 
-scrollToTopButton.addEventListener("click", function () {
-    // Functia scrollTo este o functie predefinita.
-    window.scrollTo({
-      bottom: 0,
-      behavior: "smooth",
-    });
-  });
+// Scroll To Section
+let contentAbout = document.querySelector(".scrollToAbout");
+function scrollToAbout() {
+  contentAbout.scrollIntoView({ behavior: "smooth" });
+}
 
-  document.addEventListener("scroll", function () {
-    // Proprietatea scrollY reprezinta numarul de pizeli scrollati pe axa OY (cand nu am scrollat deloc este 0).
-    if (window.scrollY > 500) {
-      btnLetBegin.style.visibility = "visible";
-    } else {
-      btnLetBegin.style.visibility = "hidden";
-    }
-  });
+// Disapear Btn
+let btnLetBegin = document.querySelector(".btn-let-begin");
+document.addEventListener("scroll", function () {
+  // Proprietatea scrollY reprezinta numarul de pizeli scrollati pe axa OY (cand nu am scrollat deloc este 0).
+  if (window.scrollY > 400) {
+    btnLetBegin.style.visibility = "visible";
+  } else {
+    btnLetBegin.style.visibility = "hidden";
+  }
+});
 
+// Button To Top - Right
+let btnToTop = document.querySelector(".btn-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
+    btnToTop.style.display = "block";
+  } else {
+    btnToTop.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
-  function Map1234() {  
-    var mapOpt1 = {  
-      center: new google.maps.LatLng(51.508742,-0.120850),  
-      zoom:9,  
-      mapTypeId: google.maps.MapTypeId.ROADMAP  
-    };   
-    var x = new google.maps.Map(document.getElementById("Map1"),mapOpt1);  
-  }  
+// Map
+function Map1234() {
+  var mapOpt1 = {
+    center: new google.maps.LatLng(51.508742, -0.12085),
+    zoom: 9,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+  var x = new google.maps.Map(document.getElementById("Map1"), mapOpt1);
+}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 let headerWelcome = document.querySelector(".header-welcome");
 let headerTyped = document.querySelector(".header-typed");
 let btnHome = document.querySelector(".btn-home");
@@ -8,45 +7,41 @@ let fgImg3 = document.querySelector(".fg3-showcase-img");
 // let fgImg4 = document.querySelector(".fg4-showcase-img");
 // let fgImg5 = document.querySelector(".fg5-showcase-img");
 
-let contentSection = document.querySelector('.content-section') 
-
+let contentSection = document.querySelector(".content-section");
 
 // Parallax Effect
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
 
   headerWelcome.style.marginBottom = value * 1 + "px";
-  headerTyped.style.marginBottom = value * 1 + "px"
+  headerTyped.style.marginBottom = value * 1 + "px";
   btnHome.style.marginTop = value * 0 + "px";
-  
+
   fgImg1.style.marginTop = value * 0.3 + "px";
   fgImg3.style.marginLeft = value * 0.3 + "px";
-  
+
   // fgImg4.style.marginRight = value * 2 + "px";
   // fgImg5.style.marginLeft = value * 3 + "px";
 });
 
 
 
-// Button Let Begin Right
-let btnLetBegin = document.querySelector('.btn-let-begin')
-document.addEventListener("scroll", function () {
-  // Proprietatea scrollY reprezinta numarul de pizeli scrollati pe axa OY (cand nu am scrollat deloc este 0).
-  if (window.scrollY > 400) {
-      btnLetBegin.style.visibility = "visible";
-  } else {
-      btnLetBegin.style.visibility = "hidden";
-  }
-});
+
+
+
+
+
+
 
 
 //Typing Section
-var typed = new Typed ('#typed-strings', {
-  strings:['Start exercising today!','No more excuses!','You are ready!'],
+var typed = new Typed("#typed-strings", {
+  strings: ["Start exercising today!", "No more excuses!", "You are ready!"],
   typeSpeed: 70,
   backSpeed: 50,
-  loop:true
+  loop: true,
 });
+
 
 
 // Disappear of text
@@ -59,6 +54,17 @@ document.addEventListener("scroll", function () {
   }
 });
 
+
+
+// Button Let Begin - Right
+let btnLetBegin = document.querySelector(".btn-let-begin");
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 400) {
+    btnLetBegin.style.visibility = "visible";
+  } else {
+    btnLetBegin.style.visibility = "hidden";
+  }
+});
 //Dissapear of Btn
 document.addEventListener("scroll", function () {
   // Proprietatea scrollY reprezinta numarul de pizeli scrollati pe axa OY (cand nu am scrollat deloc este 0).
@@ -68,6 +74,29 @@ document.addEventListener("scroll", function () {
     headerTyped.style.visibility = "visible";
   }
 });
+
+
+
+// Button To Top - Right
+let btnToTop = document.querySelector(".btn-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
+    btnToTop.style.display = "block";
+  } else {
+    btnToTop.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 
 // Scroll To Section
 function scrollToContent() {
