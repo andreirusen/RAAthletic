70a4633693015561949dae6fc99e22f9ffe49b0a
Volkerus <andrei.rusen54@gmail.com>
2023-05-11 23:00:53 +0300

    cards update

--- js/programs.js
+++ js/programs.js
@@ -12,11 +12,11 @@ function check() {
 
   for (var i = 0; i < text1.length; i++) {
     if (checkBox.checked == true) {
-      text1[i].style.display = "block";
-      text2[i].style.display = "none";
-    } else if (checkBox.checked == false) {
       text1[i].style.display = "none";
       text2[i].style.display = "block";
+    } else if (checkBox.checked == false) {
+      text1[i].style.display = "block";
+      text2[i].style.display = "none";
     }
   }
 }
