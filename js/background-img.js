5eba2f8a323f2c408d4c047d1f2f2fbcb1930416
Volkerus <andrei.rusen54@gmail.com>
2023-05-12 22:59:16 +0300

    cookies introduction

--- js/background-img.js
+++ js/background-img.js
@@ -58,3 +58,14 @@ function reveal() {
     }
   }
 }
+
+// Cookie
+console.clear();
+"use strict";
+
+(function() {
+   var offcanvas = document.getElementById("offcanvas"),
+       bs_offcanvas = new bootstrap.Offcanvas(offcanvas);
+   
+   bs_offcanvas.show();
+})();
