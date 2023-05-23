52540d27231c6390c10b2e3b6f08b757be82cdcd
Andrei Rusen <andrei.rusen54@gmail.com>
2023-05-23 13:13:34 +0300

    loading page

--- js/background-img.js
+++ js/background-img.js
@@ -1,3 +1,9 @@
+// Loader
+var preloader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  preloader.style.display = "none";
+});
+
 // Navbar
 const navEL = document.querySelector(".navbar");
 let lastScrollY = window.scrollY;
@@ -74,16 +80,15 @@ function reveal() {
 
 // Cookie
 console.clear();
-"use strict";
+("use strict");
 
 (function () {
   var offcanvas = document.getElementById("offcanvas");
-  
+
   if (!sessionStorage.getItem("offcanvasShown")) {
     var bs_offcanvas = new bootstrap.Offcanvas(offcanvas);
     bs_offcanvas.show();
-    
+
     sessionStorage.setItem("offcanvasShown", true);
   }
 })();
-
