c704f743991c1341c9e6a46febd36a3f4912c1f3
Volkersen <123008787+andreirusen@users.noreply.github.com>
2023-05-24 18:31:02 +0300

    new login section

--- account/account.js
+++ account/account.js
@@ -1,6 +1,20 @@
+// Login Form
+const signInBtnLink = document.querySelector(".logInBtn-link");
+const signUpBtnLink = document.querySelector(".signUpBtn-link");
+const wrapperLogin = document.querySelector(".wrapper-account");
+
+// SignUp Link
+signUpBtnLink.addEventListener("click", () => {
+  wrapperLogin.classList.toggle("active");
+});
+// Login Link
+signInBtnLink.addEventListener("click", () => {
+  wrapperLogin.classList.toggle("active");
+});
+
 // Add event listeners to the login and sign up buttons
-document.getElementById('login-btn').addEventListener('click', handleLogin);
-document.getElementById('signup-btn').addEventListener('click', handleSignUp);
+document.getElementById("login-btn").addEventListener("click", handleLogin);
+document.getElementById("signup-btn").addEventListener("click", handleSignUp);
 // Check if the email is valid
 function isValidEmail(email) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -8,7 +22,9 @@ function isValidEmail(email) {
 
 // Check if the password meets the requirements
 function isValidPassword(password) {
-  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(password);
+  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(
+    password
+  );
 }
 
 // Function to display error messages
@@ -17,200 +33,104 @@ function showErrorMessage(element, message) {
 }
 
 // Function to clear error messages
-function clearErrorMessage(element) {
-  element.innerText = '';
+function clearErrorMessage(element) {  
+  element.innerText = "";
 }
 
 // Function to handle login
 function handleLogin() {
-  const email = document.getElementById('login-email').value;
-  const password = document.getElementById('login-password').value;
-  const loginError = document.getElementById('login-error');
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-password").value;
+  const loginError = document.getElementById("login-error");
+  const rememberCheckbox = document.getElementById("remember-checkbox");
 
   clearErrorMessage(loginError);
 
   if (!isValidEmail(email)) {
-    showErrorMessage(loginError, 'Invalid email format.');
+    showErrorMessage(loginError, "Invalid email format.");
     return;
   }
 
   if (password.length < 6) {
-    showErrorMessage(loginError, 'Password must be at least 6 characters.');
+    showErrorMessage(loginError, "Password must be at least 6 characters.");
     return;
   }
 
   // Check if the account exists in local storage
-  const accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+  const accounts = JSON.parse(localStorage.getItem("accounts")) || [];
   const account = accounts.find((acc) => acc.email === email);
 
   if (account) {
     if (account.password === password) {
       // Redirect to the dashboard page
-      window.location.href = 'dashboard.html';
+      window.location.href = "dashboard.html";
+
+      // Remember the email if the checkbox is checked
+      if (rememberCheckbox.checked) {
+        localStorage.setItem("rememberedEmail", email);
+      } else {
+        localStorage.removeItem("rememberedEmail");
+      }
     } else {
-      showErrorMessage(loginError, 'Incorrect password.');
+      showErrorMessage(loginError, "Incorrect password.");
     }
   } else {
-    showErrorMessage(loginError, 'Account does not exist.');
+    showErrorMessage(loginError, "Account does not exist.");
   }
 }
+
+// Pre-fill the email field with remembered email, if available
+window.addEventListener("DOMContentLoaded", () => {
+  const rememberedEmail = localStorage.getItem("rememberedEmail");
+  if (rememberedEmail) {
+    document.getElementById("login-email").value = rememberedEmail;
+  }
+});
 
 // Function to handle sign up
 function handleSignUp() {
-  const email = document.getElementById('signup-email').value;
-  const password = document.getElementById('signup-password').value;
-  const repeatPassword = document.getElementById('signup-repeat-password').value;
-  const signupError = document.getElementById('signup-error');
+  const email = document.getElementById("signup-email").value;
+  const password = document.getElementById("signup-password").value;
+  const repeatPassword = document.getElementById(
+    "signup-repeat-password"
+  ).value;
+  const signupError = document.getElementById("signup-error");
 
   clearErrorMessage(signupError);
 
   if (!isValidEmail(email)) {
-    showErrorMessage(signupError, 'Invalid email format.');
+    showErrorMessage(signupError, "Invalid email format.");
     return;
   }
 
   if (password.length < 6) {
-    showErrorMessage(signupError, 'Password must be at least 6 characters.');
+    showErrorMessage(signupError, "Password must be at least 6 characters.");
     return;
   }
 
   if (password !== repeatPassword) {
-    showErrorMessage(signupError, 'Passwords do not match.');
+    showErrorMessage(signupError, "Passwords do not match.");
     return;
   }
 
   // Check if the account already exists in local storage
-  const accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+  const accounts = JSON.parse(localStorage.getItem("accounts")) || [];
   const existingAccount = accounts.find((acc) => acc.email === email);
 
   if (existingAccount) {
-    showErrorMessage(signupError, 'Account already exists.');
+    showErrorMessage(signupError, "Account already exists.");
     return;
   }
 
   // Create a new account and save it to local storage
   const newAccount = {
     email: email,
-    password: password
+    password: password,
   };
 
   accounts.push(newAccount);
-  localStorage.setItem('accounts', JSON.stringify(accounts));
+  localStorage.setItem("accounts", JSON.stringify(accounts));
 
   // Redirect to the dashboard page
-  window.location.href = 'dashboard.html';
-}
-
-
-
-
-
-
-
-
-
-// Mobile
-// Mobile
-// Account local storage - Add event listeners to the login and sign up buttons
-document.getElementById('mobile-login-btn').addEventListener('click', handleMobileLogin);
-document.getElementById('mobile-signup-btn').addEventListener('click', handleMobileSignUp);
-
-// Check if the email is valid
-function isValidEmailMobile(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-// Check if the password meets the requirements
-function isValidPasswordMobile(password) {
-  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(password);
-}
-
-// Function to display error messages
-function showErrorMessageMobile(element, message) {
-  element.innerText = message;
-}
-
-// Function to clear error messages
-function clearErrorMessageMobile(element) {
-  element.innerText = '';
-}
-
-// Function to handle login
-function handleMobileLoginMobile() {
-  const email = document.getElementById('mobile-login-email').value;
-  const password = document.getElementById('mobile-login-password').value;
-  const loginError = document.getElementById('mobile-login-error');
-
-  clearErrorMessage(loginError);
-
-  if (!isValidEmail(email)) {
-    showErrorMessage(loginError, 'Invalid email format.');
-    return;
-  }
-
-  if (password.length < 6) {
-    showErrorMessage(loginError, 'Password must be at least 6 characters.');
-    return;
-  }
-
-  // Check if the account exists in local storage
-  const accounts = JSON.parse(localStorage.getItem('mobile-accounts')) || [];
-  const account = accounts.find((acc) => acc.email === email);
-
-  if (account) {
-    if (account.password === password) {
-      // Redirect to the dashboard page
-      window.location.href = 'mobile-dashboard.html';
-    } else {
-      showErrorMessage(loginError, 'Incorrect password.');
-    }
-  } else {
-    showErrorMessage(loginError, 'Account does not exist.');
-  }
-}
-
-// Function to handle sign up
-function handleMobileSignUpMobile() {
-  const email = document.getElementById('mobile-signup-email').value;
-  const password = document.getElementById('mobile-signup-password').value;
-  const repeatPassword = document.getElementById('mobile-signup-repeat-password').value;
-  const signupError = document.getElementById('mobile-signup-error');
-
-  clearErrorMessage(signupError);
-
-  if (!isValidEmail(email)) {
-    showErrorMessage(signupError, 'Invalid email format.');
-    return;
-  }
-
-  if (password.length < 6) {
-    showErrorMessage(signupError, 'Password must be at least 6 characters.');
-    return;
-  }
-
-  if (password !== repeatPassword) {
-    showErrorMessage(signupError, 'Passwords do not match.');
-    return;
-  }
-
-  // Check if the account already exists in local storage
-  const accounts = JSON.parse(localStorage.getItem('mobile-accounts')) || [];
-  const existingAccount = accounts.find((acc) => acc.email === email);
-
-  if (existingAccount) {
-    showErrorMessage(signupError, 'Account already exists.');
-    return;
-  }
-
-  // Create a new account and save it to local storage
-  const newAccount = {
-    email: email,
-    password: password
-  };
-
-  accounts.push(newAccount);
-  localStorage.setItem('mobile-accounts', JSON.stringify(accounts));
-
-  // Redirect to the dashboard page
-  window.location.href = 'mobile-dashboard.html';
+  window.location.href = "dashboard.html";
 }
