a8ab24e2d8f8a94c401a4499bcc9092e39059ab3
Andrei Rusen <123008787+andreirusen@users.noreply.github.com>
2023-05-25 20:05:59 +0300

    uppdate account functions

--- account/account.js
+++ account/account.js
@@ -1,8 +1,8 @@
-// Login Form
+// Login Form Switch
+// Switch between signUp and Login
 const signInBtnLink = document.querySelector(".logInBtn-link");
 const signUpBtnLink = document.querySelector(".signUpBtn-link");
 const wrapperLogin = document.querySelector(".wrapper-account");
-
 // SignUp Link
 signUpBtnLink.addEventListener("click", () => {
   wrapperLogin.classList.toggle("active");
@@ -11,31 +11,6 @@ signUpBtnLink.addEventListener("click", () => {
 signInBtnLink.addEventListener("click", () => {
   wrapperLogin.classList.toggle("active");
 });
-
-// Add event listeners to the login and sign up buttons
-document.getElementById("login-btn").addEventListener("click", handleLogin);
-document.getElementById("signup-btn").addEventListener("click", handleSignUp);
-// Check if the email is valid
-function isValidEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-// Check if the password meets the requirements
-function isValidPassword(password) {
-  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(
-    password
-  );
-}
-
-// Function to display error messages
-function showErrorMessage(element, message) {
-  element.innerText = message;
-}
-
-// Function to clear error messages
-function clearErrorMessage(element) {
-  element.innerText = "";
-}
 
 // Function to handle login
 function handleLogin() {
@@ -79,14 +54,6 @@ function handleLogin() {
   }
 }
 
-// Pre-fill the email field with remembered email, if available
-window.addEventListener("DOMContentLoaded", () => {
-  const rememberedEmail = localStorage.getItem("rememberedEmail");
-  if (rememberedEmail) {
-    document.getElementById("login-email").value = rememberedEmail;
-  }
-});
-
 // Function to handle sign up
 function handleSignUp() {
   const email = document.getElementById("signup-email").value;
@@ -103,13 +70,17 @@ function handleSignUp() {
     return;
   }
 
-  if (password.length < 6) {
-    showErrorMessage(signupError, "Password must be at least 6 characters.");
+  if (!isValidPassword(password, signupError)) {
     return;
   }
 
   if (password !== repeatPassword) {
     showErrorMessage(signupError, "Passwords do not match.");
+    return;
+  }
+
+  if (password.length < 6) {
+    showErrorMessage(signupError, "Password must be at least 6 characters.");
     return;
   }
 
@@ -135,3 +106,63 @@ function handleSignUp() {
   // Redirect to the dashboard page
   window.location.href = "dashboard.html";
 }
+
+// Function to display error messages
+function showErrorMessage(element, message) {
+  element.innerText = message;
+}
+
+// Function to clear error messages
+function clearErrorMessage(element) {
+  element.innerText = "";
+}
+
+// Check if the email is valid
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+// Check if the password meets the requirements
+function isValidPassword(password, errorElement) {
+  if (password.length < 6) {
+    showErrorMessage(errorElement, "Password must be at least 6 characters.");
+    return false;
+  }
+
+  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(
+    password
+  );
+}
+
+// Pre-fill the email field with remembered email, if available
+window.addEventListener("DOMContentLoaded", () => {
+  const rememberedEmail = localStorage.getItem("rememberedEmail");
+  if (rememberedEmail) {
+    document.getElementById("login-email").value = rememberedEmail;
+  }
+});
+
+// Eye Password
+pwShowHide = document.querySelectorAll(".eye-icon");
+links = document.querySelectorAll(".link");
+
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    let pwFields =
+      eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+    pwFields.forEach((password) => {
+      if (password.type === "password") {
+        password.type = "text";
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+    });
+  });
+});
+
+// Add event listeners to the login and sign up buttons
+document.getElementById("login-btn").addEventListener("click", handleLogin);
+document.getElementById("signup-btn").addEventListener("click", handleSignUp);
