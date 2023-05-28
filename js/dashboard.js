a71690298b7e418bb286a35ba14522310ac5cde9
Andrei Rusen <123008787+andreirusen@users.noreply.github.com>
2023-05-28 12:22:46 +0300

    dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,105 +1,148 @@
-const navItems = document.querySelectorAll('.side-nav__item');
-const removeClasses = () => {
-  navItems.forEach(eachItem => {
-    eachItem.classList.remove('side-nav__item-active');
-  });
+// Loader
+var preloader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  preloader.style.display = "none";
+});
+
+// Navbar
+const navEL = document.querySelector(".navbar");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", function () {
+  // Navbar
+  if (lastScrollY < window.scrollY) {
+    navEL.classList.add("navbar--hidden");
+  } else {
+    navEL.classList.remove("navbar--hidden");
+  }
+  lastScrollY = window.scrollY;
+});
+
+// Button To Top - Right
+let btnToTop = document.querySelector(".btn-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
+    btnToTop.style.display = "block";
+  } else {
+    btnToTop.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-navItems.forEach(eachItem => {
-  eachItem.addEventListener('click', function() {
-      removeClasses();
-    eachItem.classList.add('side-nav__item-active');
+// Disapear Btn
+let btnLetBegin = document.querySelector(".btn-let-begin");
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 400) {
+    btnLetBegin.style.visibility = "visible";
+  } else {
+    btnLetBegin.style.visibility = "hidden";
+  }
+});
+
+// Reveal Section
+window.addEventListener("scroll", reveal);
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowheight = window.innerHeight;
+    let revealtop = reveals[i].getBoundingClientRect().top;
+    let revealpoint = 40;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+// Cookie
+console.clear();
+("use strict");
+(function () {
+  var offcanvas = document.getElementById("offcanvas");
+
+  if (!sessionStorage.getItem("offcanvasShown")) {
+    var bs_offcanvas = new bootstrap.Offcanvas(offcanvas);
+    bs_offcanvas.show();
+
+    sessionStorage.setItem("offcanvasShown", true);
+  }
+})();
+
+
+/**
+ * Created by Kupletsky Sergey on 16.09.14.
+ *
+ * Hierarchical timing
+ * Add specific delay for CSS3-animation to elements.
+ */
+
+(function($) {
+  var speed = 2000;
+  var container =  $('.display-animation');
+  container.each(function() {
+      var elements = $(this).children();
+      elements.each(function() {
+          var elementOffset = $(this).offset();
+          var offset = elementOffset.left*0.8 + elementOffset.top;
+          var delay = parseFloat(offset/speed).toFixed(2);
+          $(this)
+              .css("-webkit-animation-delay", delay+'s')
+              .css("-o-animation-delay", delay+'s')
+              .css("animation-delay", delay+'s')
+              .addClass('animated');
+      });
   });
-});
+})(jQuery);
 
-const ctx = document.getElementById('myChart');
+/**
+* Created by Kupletsky Sergey on 04.09.14.
+*
+* Ripple-effect animation
+* Tested and working in: ?IE9+, Chrome (Mobile + Desktop), ?Safari, ?Opera, ?Firefox.
+* JQuery plugin add .ink span in element with class .ripple-effect
+* Animation work on CSS3 by add/remove class .animate to .ink span
+*/
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Des'],
-    datasets: [{
-      label: 'Expense',
-      data: [11, 3, 14, 7, 4, 15, 7, 9, 15, 13, 7, 14],
-      borderWidth: 1,
-      borderRadius: 30,
-      barThickness: 12,
-      backgroundColor: [
-        'rgba(114, 92, 255, 1)'
-      ],
-      borderColor: [
-        'rgba(114, 92, 255, 1)'
-      ],
-      hoverBackgroundColor: [
-        'rgba(28, 30, 35, 1)'
-      ],
-      hoverBorderColor: [
-        'rgba(28, 30, 35, 1)'
-      ],
-    }]
-  },
-  options: {
-    responsive: true,
-    scales: {
-      y: {
-        beginAtZero: true,
-        ticks: {
-            // Include a dollar sign in the ticks
-            callback: function(value, index, ticks) {
-                return '$' + value + 'k';
-            },
-            stepSize: 5,
-        },
-      },
-      x: {
-        grid: {
-            display: false
-        }
+(function($) {
+  $(".ripple-effect").click(function(e){
+      var rippler = $(this);
+
+      // create .ink element if it doesn't exist
+      if(rippler.find(".ink").length == 0) {
+          rippler.append("<span class='ink'></span>");
       }
-    },
-    plugins: {
-        legend: {
-          display:false,
-          labels: {
-            font: {
-                size: 12,
-                family: "'Plus Jakarta Sans', sans-serif",
-                lineHeight: 18,
-                weight: 600
-            }
-          }
-        }
-    }
-  }
-});
 
-const ctx2 = document.getElementById('myChart2');
+      var ink = rippler.find(".ink");
 
-new Chart(ctx2, {
-  type: 'doughnut',
-  data: {
-    datasets: [{
-      label: 'Overall Spending',
-      data: [8000, 2130, 1510, 2245, 4385, 1000],
-      borderRadius: 5,
-      cutout: 80,
-      backgroundColor: [
-        'rgb(235, 124, 166)',
-        'rgb(255, 172, 200)',
-        'rgb(204, 111, 248)',
-        'rgb(124, 92, 252)',
-        'rgb(92, 175, 252)',
-        'rgb(161, 169, 254)'
-      ],
-      hoverOffset: 4,
-      spacing: 8
-    }]
-  },
-  options: {
-    plugins: {
-      legend: {
-        display: false
+      // prevent quick double clicks
+      ink.removeClass("animate");
+
+      // set .ink diametr
+      if(!ink.height() && !ink.width())
+      {
+          var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
+          ink.css({height: d, width: d});
       }
-    }
-  }
-});
+
+      // get click coordinates
+      var x = e.pageX - rippler.offset().left - ink.width()/2;
+      var y = e.pageY - rippler.offset().top - ink.height()/2;
+
+      // set .ink position and add class .animate
+      ink.css({
+          top: y+'px',
+          left:x+'px'
+      }).addClass("animate");
+  })
+})(jQuery);
