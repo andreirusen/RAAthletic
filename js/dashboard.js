e5eba5916cb8d5c71330f1849f43e48ecf7e7aff
Volkersen <123008787+andreirusen@users.noreply.github.com>
2023-06-03 14:59:50 +0300

    dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,8 +6,7 @@ window.addEventListener("load", function () {
 
 // Navbar
 const navEL = document.querySelector(".navbar");
-let lastScrollY = window.scrollY;
-
+var lastScrollY = window.scrollY;
 window.addEventListener("scroll", function () {
   // Navbar
   if (lastScrollY < window.scrollY) {
@@ -19,7 +18,7 @@ window.addEventListener("scroll", function () {
 });
 
 // Button To Top - Right
-let btnToTop = document.querySelector(".btn-to-top");
+var btnToTop = document.querySelector(".btn-to-top");
 window.onscroll = function () {
   scrollFunction();
 };
@@ -39,7 +38,7 @@ function topFunction() {
 }
 
 // Disapear Btn
-let btnLetBegin = document.querySelector(".btn-let-begin");
+var btnLetBegin = document.querySelector(".btn-let-begin");
 document.addEventListener("scroll", function () {
   if (window.scrollY > 400) {
     btnLetBegin.style.visibility = "visible";
@@ -51,7 +50,7 @@ document.addEventListener("scroll", function () {
 // Reveal Section
 window.addEventListener("scroll", reveal);
 function reveal() {
-  let reveals = document.querySelectorAll(".reveal");
+  var reveals = document.querySelectorAll(".reveal");
   for (let i = 0; i < reveals.length; i++) {
     let windowheight = window.innerHeight;
     let revealtop = reveals[i].getBoundingClientRect().top;
@@ -79,85 +78,19 @@ console.clear();
   }
 })();
 
-
 // Scroll To Timer
-const scrollDelay = 10;
-const scrollStepDelay = 10;
-const targetElement = document.getElementById("selected-section");
+const scrollDelay = 1000;
+const scrollStepDelay = 1000;
+const targetElement = document.getElementById("dashboard-section");
 
 function scrollTo() {
   targetElement.scrollIntoView({ behavior: "smooth" });
 }
-
 setTimeout(scrollTo, scrollDelay);
 
-
-
-//  Main DashBoard
-
-(function ($) {
-  var speed = 2000;
-  var container = $(".display-animation");
-  container.each(function () {
-    var elements = $(this).children();
-    elements.each(function () {
-      var elementOffset = $(this).offset();
-      var offset = elementOffset.left * 0.8 + elementOffset.top;
-      var delay = parseFloat(offset / speed).toFixed(2);
-      $(this)
-        .css("-webkit-animation-delay", delay + "s")
-        .css("-o-animation-delay", delay + "s")
-        .css("animation-delay", delay + "s")
-        .addClass("animated");
-    });
-  });
-})(jQuery);
-
-(function ($) {
-  $(".ripple-effect").click(function (e) {
-    var rippler = $(this);
-
-    // create .ink element if it doesn't exist
-    if (rippler.find(".ink").length == 0) {
-      rippler.append("<span class='ink'></span>");
-    }
-
-    var ink = rippler.find(".ink");
-
-    // prevent quick double clicks
-    ink.removeClass("animate");
-
-    // set .ink diametr
-    if (!ink.height() && !ink.width()) {
-      var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
-      ink.css({ height: d, width: d });
-    }
-
-    // get click coordinates
-    var x = e.pageX - rippler.offset().left - ink.width() / 2;
-    var y = e.pageY - rippler.offset().top - ink.height() / 2;
-
-    // set .ink position and add class .animate
-    ink
-      .css({
-        top: y + "px",
-        left: x + "px",
-      })
-      .addClass("animate");
-  });
-})(jQuery);
-
-
-
-
-
-// Get the calendar container
+// Calendar container
 var calendarContainer = document.getElementById("calendar");
-
-// Create a new date object
 var date = new Date();
-
-// Display the calendar
 displayCalendar(date);
 
 // Function to display the calendar for a given date
@@ -169,7 +102,7 @@ function displayCalendar(date) {
   // Set the calendar title and select inputs
   var title = "<h2>" + getMonthName(month) + " " + year + "</h2>";
   var selectInputs =
-    "<div>" +
+    "<div class='calendars-select'>" +
     "<select id='month' onchange='changeCalendar()'>" +
     generateMonthOptions(month) +
     "</select>" +
@@ -181,7 +114,7 @@ function displayCalendar(date) {
   // Generate the calendar table
   var table = "<table>";
 
-  // Generate the table headers (days of the week)
+  // Generate the table headers
   table += "<tr>";
   table += "<th>Sun</th>";
   table += "<th>Mon</th>";
@@ -477,3 +410,26 @@ function goToToday() {
     currentDayCell.classList.add("current-day");
   }
 }
+
+// Profile IMG
+const image = document.querySelector(".profile-img");
+const input = document.querySelector(".profile-input");
+
+input.addEventListener("change", () => {
+  image.src = URL.createObjectURL(input.files[0]);
+
+  // Save the image to local storage
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const imageData = e.target.result;
+    localStorage.setItem("profilePicture", imageData);
+  };
+  reader.readAsDataURL(input.files[0]);
+});
+// Retrieve the saved profile picture from local storage
+window.addEventListener("DOMContentLoaded", () => {
+  const savedImage = localStorage.getItem("profilePicture");
+  if (savedImage) {
+    image.src = savedImage;
+  }
+});
