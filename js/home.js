a44851a049555884a8e21a8b702968f2f8c9e269
Volkersen <123008787+andreirusen@users.noreply.github.com>
2023-06-07 19:03:01 +0300

    review section

--- js/home.js
+++ js/home.js
@@ -20,6 +20,7 @@ var typed = new Typed("#typed-strings", {
   loop: true,
 });
 
+// Google Map
 let map;
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
@@ -36,3 +37,35 @@ async function initMap() {
   })
 }
 initMap();
+
+
+// Carousel
+const multipleItemCarousel = document.querySelector("#carouselExampleControls");
+
+if (window.matchMedia("(min-width:576px)").matches) {
+  const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+    interval: false
+  });
+
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+
+  var scrollPosition = 0;
+
+  $(".carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      console.log("next");
+      scrollPosition = scrollPosition + cardWidth;
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+    }
+  });
+  $(".carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      console.log("prev");
+      scrollPosition = scrollPosition - cardWidth;
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+    }
+  });
+} else {
+  $(multipleItemCarousel).addClass("slide");
+}
