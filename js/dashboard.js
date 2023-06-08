3487863bfff8f8317f2709a625df6ce889d8e773
Volkersen <123008787+andreirusen@users.noreply.github.com>
2023-06-08 23:20:39 +0300

    update dashboard css

--- js/dashboard.js
+++ js/dashboard.js
@@ -453,3 +453,31 @@ window.addEventListener("DOMContentLoaded", () => {
     imageNav.src = savedImage;
   }
 });
+
+
+
+// Ripple Effect
+var buttons = document.getElementsByClassName('ripple-effect');
+
+Array.prototype.forEach.call(buttons, function(b){
+  b.addEventListener('click', createRipple);
+})
+
+function createRipple(e)
+{
+  if(this.getElementsByClassName('ripple').length > 0)
+    {
+      this.removeChild(this.childNodes[1]);
+    }
+  
+  var circle = document.createElement('div');
+  this.appendChild(circle);
+  
+  var d = Math.max(this.clientWidth, this.clientHeight);
+  circle.style.width = circle.style.height = d + 'px';
+  
+  circle.style.left = e.clientX - this.offsetLeft - d / 2 + 'px';
+  circle.style.top = e.clientY - this.offsetTop - d / 2 + 'px';
+  
+  circle.classList.add('ripple');
+}
