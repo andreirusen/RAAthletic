af1e6ea645e4f3084af2723d4ea71daa5320c2ce
Andrei Rusen <123008787+andreirusen@users.noreply.github.com>
2023-07-06 17:27:16 +0300

    new programs cards

--- js/programs.js
+++ js/programs.js
@@ -26,3 +26,37 @@ function scrollTo() {
 }
 
 setTimeout(scrollTo, scrollDelay);
+
+
+// Rating Carousel
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  speed: 600,
+  loop: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: false,
+  },
+});
+
+// Timer to change the slider every 4 seconds
+setInterval(function() {
+  swiper.slideNext();
+}, 4000);
+
+
+
